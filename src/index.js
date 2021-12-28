702fb34876e1948821e6b8ab1cfaa331af0a6cfa
Nozeil <nozeilwrk@gmail.com>
2021-12-28 16:58:36 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let answerStr = '';
+	for (let i = String(n).length - 1; i >= 0; i--) {
+		answerStr += String(n)[i];
+	}
+	return (answerStr[answerStr.length - 1] === '-') ? answerStr.slice(0, -1) : answerStr;
 }
